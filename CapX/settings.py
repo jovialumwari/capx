10622aded4bd2b2c903898925a43ba146de2c5e5
jovialumwari <umwarijovial@gmail.com>
2023-10-12 16:35:05 +0300

    Added 'core' application for managing landing and related pages.

--- CapX/settings.py
+++ CapX/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 from .settings_local import *
 
@@ -19,6 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Application definition
 
 INSTALLED_APPS = [
+    'Core.apps.CoreConfig',
     'user_profile.apps.UserProfileConfig',
     'skills.apps.SkillsConfig',
     'map.apps.MapConfig',
@@ -46,7 +48,7 @@ ROOT_URLCONF = 'CapX.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR, 'templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -108,6 +110,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
