7ca07d0c512b35832fd32d7a1a6e6a588de0ba66
Éder Porto <eder.porto@hotmail.com>
2023-10-19 15:13:28 -0300

    Updating settings in order to allow Toolforge and database integration

--- CapX/settings.py
+++ CapX/settings.py
@@ -13,9 +13,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 from .settings_local import *
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -60,18 +57,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'CapX.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
